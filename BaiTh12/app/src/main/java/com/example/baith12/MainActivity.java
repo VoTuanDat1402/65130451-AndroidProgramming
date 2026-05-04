c761b06266d5f4e43673e756f1cd7859e56dd8e7
Vo Tuan Dat <dattuanvo1402@gmail.com>
2026-05-04 10:24:14 +0700

    BAITH12

---
+++ BaiTh12/app/src/main/java/com/example/baith12/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.baith12;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        if (savedInstanceState == null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            ContentFragment contentFragment = new ContentFragment();
+            FooterFragment footerFragment = new FooterFragment();
+
+            fragmentTransaction.replace(R.id.fragment_content, contentFragment);
+            fragmentTransaction.replace(R.id.fragment_footer, footerFragment);
+            fragmentTransaction.commit();
+        }
+    }
+}
